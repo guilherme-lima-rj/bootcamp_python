fa8bfa8edda3369e9aa6c064a953907d3620928b
Guilherme Lima <guilherme.lima@ymail.com>
2024-08-06 17:02:44 -0300

    adicionando desafio.py atualizado em aula01

--- aula01/desafio.py
+++ aula01/desafio.py
@@ -1,9 +1,24 @@
 # Instruções:
 # O programa deve começar solicitando ao usuário que insira seu nome.
-# Em seguida, o programa deve pedir ao usuário para inserir o valor do seu salário. Considere que este valor pode ser um número decimal.
-# Depois, o programa deve solicitar a porcentagem do bônus recebido pelo usuário, que também pode ser um número decimal.
+nome_usuario = input("Digite seu nome: ")
+
+# Em seguida, o programa deve pedir ao usuário para inserir o valor do seu salário. 
+# Considere que este valor pode ser um número decimal.
+salario_usuario = float(input("Digite seu salário: "))
+
+# Depois, o programa deve solicitar a porcentagem do bônus recebido pelo usuário, 
+# que também pode ser um número decimal.
+bonus_usuario = float(input("Digite seu percentual de bônus recebido: "))
+
 # O cálculo do KPI do bônus de 2024 é de 1000 + salario * bônus
-# Finalmente, o programa deve imprimir uma mensagem no seguinte formato: "Olá [nome], o seu valor bônus foi de 5000".
+CONSTANTE_BONUS = 1000
+
+valor_bonus = CONSTANTE_BONUS + salario_usuario * bonus_usuario
+
+# Finalmente, o programa deve imprimir uma mensagem no seguinte formato: 
+# "Olá [nome], o seu valor bônus foi de 5000".
+print(f"Olá {nome_usuario} o seu valor bônus foi de {valor_bonus}.")
+
 # Exemplo de Saída:
 # Se o usuário digitar "Luciano" como nome, "5000" como salário e "1.5" como bônus, o programa deve imprimir:
 
