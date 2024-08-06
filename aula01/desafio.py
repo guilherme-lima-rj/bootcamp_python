831500251cc9532e98b18e781721407d80af95bc
Guilherme Lima <guilherme.lima@ymail.com>
2024-08-06 16:47:40 -0300

    adicionando folder  aula01.

---
+++ aula01/desafio.py
@@ -0,0 +1,10 @@
+# Instruções:
+# O programa deve começar solicitando ao usuário que insira seu nome.
+# Em seguida, o programa deve pedir ao usuário para inserir o valor do seu salário. Considere que este valor pode ser um número decimal.
+# Depois, o programa deve solicitar a porcentagem do bônus recebido pelo usuário, que também pode ser um número decimal.
+# O cálculo do KPI do bônus de 2024 é de 1000 + salario * bônus
+# Finalmente, o programa deve imprimir uma mensagem no seguinte formato: "Olá [nome], o seu valor bônus foi de 5000".
+# Exemplo de Saída:
+# Se o usuário digitar "Luciano" como nome, "5000" como salário e "1.5" como bônus, o programa deve imprimir:
+
+# Olá Luciano, o seu bônus foi de 8500
---
+++ aula01/exercicio.py
@@ -0,0 +1,6 @@
+valor_1 = input("Digite um valor:")
+valor_2 = input("Digite outro valor:")
+
+resultado = int(valor_1) + int(valor_2)
+
+print(f"A soma de {valor_1} e {valor_2} é {resultado}")
